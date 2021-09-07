e5d607f932f3702c21f34169b5a090465f265a77
lailabougria <laila.bougria@particular.net>
2021-09-07 10:17:43 +0200

    custom policy

--- AzureFunctionsRecoverability/AzureFunctions.ASBTrigger.FunctionsHostBuilder/Startup.cs
+++ AzureFunctionsRecoverability/AzureFunctions.ASBTrigger.FunctionsHostBuilder/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NServiceBus;
+using NServiceBus.Transport;
+using System;
 using System.Threading.Tasks;
 
 #region configuration-with-function-host-builder
@@ -31,16 +33,35 @@ public class Startup : FunctionsStartup
         {
             var endpointConfig = new ServiceBusTriggeredEndpointConfiguration(EndpointName);
             endpointConfig.AdvancedConfiguration.EnableFeature<GrabMessageSessionFeature>();
-            endpointConfig.AdvancedConfiguration.Recoverability().Delayed(settings => settings.NumberOfRetries(0));
-            endpointConfig.AdvancedConfiguration.Recoverability().Immediate(settings => settings.NumberOfRetries(1));
-            endpointConfig.AdvancedConfiguration.Recoverability().Failed(settings => settings.OnMessageSentToErrorQueue(failedMsg =>
-            {
-                GrabMessageSessionFeature.Session?.Publish<ISomethingFailedEvent>(ev => ev.Whathappened = failedMsg.Exception.Message);
-                return Task.CompletedTask;
-            }));
+            endpointConfig.AdvancedConfiguration.Recoverability().Delayed(settings => settings.NumberOfRetries(MaxImmediateRetries));
+            endpointConfig.AdvancedConfiguration.Recoverability().Immediate(settings => settings.NumberOfRetries(MaxDelayedRetries));
+            // endpointConfig.AdvancedConfiguration.Recoverability().Failed(settings => settings.OnMessageSentToErrorQueue(failedMsg =>
+            // {
+            //     GrabMessageSessionFeature.Session?.Publish<ISomethingFailedEvent>(ev => ev.Whathappened = failedMsg.Exception.Message);
+            //     return Task.CompletedTask;
+            // }));
+            endpointConfig.AdvancedConfiguration.Recoverability().CustomPolicy(RecoverabilityPolicy);
             return endpointConfig;
         });
     }
+
+    RecoverabilityAction RecoverabilityPolicy(RecoverabilityConfig config, ErrorContext context)
+    {
+        if (context.ImmediateProcessingFailures == MaxImmediateRetries && context.DelayedDeliveriesPerformed == MaxDelayedRetries) // retries depleted
+        {
+            GrabMessageSessionFeature.Session?.Publish<ISomethingFailedEvent>(ev => ev.Whathappened = context.Exception.Message);
+            return RecoverabilityAction.Discard("Retries depleted");
+        }
+
+        if (context.ImmediateProcessingFailures < MaxImmediateRetries)
+            return RecoverabilityAction.ImmediateRetry();
+        
+        return RecoverabilityAction.DelayedRetry(TimeSpan.FromSeconds(DelayTime));
+    }
+
+    const double DelayTime = 1;
+    const int MaxDelayedRetries = 1;
+    const int MaxImmediateRetries = 2;
 }
 
 #endregion
